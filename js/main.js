f14c0c0b8a582b70edd0fb1fd08e25213f13cc4f
vin@wataidesign.com <vin@wataidesign.com>
2020-09-26 18:03:43 +0800

    save

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-// $(function () {
-//     console.log('main.js');
+$(function () {
+    console.log('main.js');
 
-//     $(".select p").click(function () {
-//         let ul = $(".select ul");
+    $(".select p").click(function () {
+        let ul = $(".select ul");
 
-//         if (ul.css("display") !== "none") {
-//             ul.addClass('none');
-//         } else {
-//             ul.removeClass('none');
-//         }
-//     });
-
-//     $(".select ul li a").click(function () {
-//         let txt = $(this).text();
-//         $(".select p").html(txt);
-//         $(".select ul").addClass('none');
-//     });
-
-// });
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    let selectP = document.querySelector('.select p');
-    let selectUl = document.querySelector('.select ul');
-    let selectA = document.querySelector('.select ul li a');
-
-    selectP.addEventListener("click", function (e) {
-        if (selectUl.getAttribute('class') !== "none") {
-            selectUl.setAttribute('class', 'none');
+        if (ul.css("display") !== "none") {
+            ul.addClass('none');
         } else {
-            selectUl.removeAttribute('class', 'none');
-        };
+            ul.removeClass('none');
+        }
     });
 
-    selectA.addEventListener("click", function (e) {
-        let txt = document.querySelector(this).textContent;
-        selectP.innerHTML = txt;
-        selectUl.setAttribute('class', 'none');
+    $(".select ul li a").click(function () {
+        let txt = $(this).text();
+        $(".select p").html(txt);
+        $(".select ul").addClass('none');
     });
+
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     let selectP = document.querySelector('.select p');
+//     let selectUl = document.querySelector('.select ul');
+//     let selectA = document.querySelector('.select ul li a');
+
+//     selectP.addEventListener("click", function (e) {
+//         if (selectUl.getAttribute('class') !== "none") {
+//             selectUl.setAttribute('class', 'none');
+//         } else {
+//             selectUl.removeAttribute('class', 'none');
+//         };
+//     });
+
+//     selectA.addEventListener("click", function (e) {
+//         let txt = document.querySelector(this).textContent;
+//         selectP.innerHTML = txt;
+//         selectUl.setAttribute('class', 'none');
+//     });
 
     // $(".select ul li a").click(function () {
     //     let txt = $(this).text();
@@ -45,5 +45,5 @@ document.addEventListener("DOMContentLoaded", function () {
     //     $(".select ul").addClass('none');
     // });
 
-});
+// });
 
