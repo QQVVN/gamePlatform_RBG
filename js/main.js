d38db3fd6c437bd8e9322f1f02820d83a5560163
vin@wataidesign.com <vin@wataidesign.com>
2020-09-28 13:22:21 +0800

    save 、 ie9 no let

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
-    console.log('main.js');
+    console.log('script main.js');
 
     $(".select p").click(function () {
-        let ul = $(".select ul");
+        var ul = $(".select ul");
 
         if (ul.css("display") !== "none") {
             ul.addClass('none');
@@ -12,7 +12,7 @@ $(function () {
     });
 
     $(".select ul li a").click(function () {
-        let txt = $(this).text();
+        var txt = $(this).text();
         $(".select p").html(txt);
         $(".select ul").addClass('none');
     });
@@ -21,9 +21,9 @@ $(function () {
 
 // document.addEventListener("DOMContentLoaded", function () {
 
-//     let selectP = document.querySelector('.select p');
-//     let selectUl = document.querySelector('.select ul');
-//     let selectA = document.querySelector('.select ul li a');
+//     var selectP = document.querySelector('.select p');
+//     var selectUl = document.querySelector('.select ul');
+//     var selectA = document.querySelector('.select ul li a');
 
 //     selectP.addEventListener("click", function (e) {
 //         if (selectUl.getAttribute('class') !== "none") {
@@ -34,16 +34,16 @@ $(function () {
 //     });
 
 //     selectA.addEventListener("click", function (e) {
-//         let txt = document.querySelector(this).textContent;
+//         var txt = document.querySelector(this).textContent;
 //         selectP.innerHTML = txt;
 //         selectUl.setAttribute('class', 'none');
 //     });
 
-    // $(".select ul li a").click(function () {
-    //     let txt = $(this).text();
-    //     $(".select p").html(txt);
-    //     $(".select ul").addClass('none');
-    // });
+//     $(".select ul li a").click(function () {
+//         var txt = $(this).text();
+//         $(".select p").html(txt);
+//         $(".select ul").addClass('none');
+//     });
 
 // });
 
