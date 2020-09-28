4cdf6308f8c30f38d1603322c099f853a265b082
vin@wataidesign.com <vin@wataidesign.com>
2020-09-28 16:19:29 +0800

    save top 1 ok

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
     console.log('script main.js');
 
+    // select js
     $(".select p").click(function () {
         var ul = $(".select ul");
 
@@ -10,11 +11,26 @@ $(function () {
             ul.removeClass('none');
         }
     });
-
     $(".select ul li a").click(function () {
         var txt = $(this).text();
         $(".select p").html(txt);
         $(".select ul").addClass('none');
+    });
+
+    // sign in
+    $(".hif .si").click(function () {
+        var hif = $(".hif");
+        var hifi = $(".hifi");
+        hif.addClass('none');
+        hifi.removeClass('none');
+    });
+
+    // sign out
+    $(".hifi .so").click(function () {
+        var hif = $(".hif");
+        var hifi = $(".hifi");
+        hifi.addClass('none');
+        hif.removeClass('none');
     });
 
 });
