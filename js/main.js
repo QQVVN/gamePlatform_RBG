858f84806826c6c8ee404a02a8e4895965ec8bcc
vin@wataidesign.com <vin@wataidesign.com>
2020-09-28 15:54:02 +0800

    hifi ok

--- js/main.js
+++ js/main.js
@@ -18,32 +18,3 @@ $(function () {
     });
 
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     var selectP = document.querySelector('.select p');
-//     var selectUl = document.querySelector('.select ul');
-//     var selectA = document.querySelector('.select ul li a');
-
-//     selectP.addEventListener("click", function (e) {
-//         if (selectUl.getAttribute('class') !== "none") {
-//             selectUl.setAttribute('class', 'none');
-//         } else {
-//             selectUl.removeAttribute('class', 'none');
-//         };
-//     });
-
-//     selectA.addEventListener("click", function (e) {
-//         var txt = document.querySelector(this).textContent;
-//         selectP.innerHTML = txt;
-//         selectUl.setAttribute('class', 'none');
-//     });
-
-//     $(".select ul li a").click(function () {
-//         var txt = $(this).text();
-//         $(".select p").html(txt);
-//         $(".select ul").addClass('none');
-//     });
-
-// });
-
